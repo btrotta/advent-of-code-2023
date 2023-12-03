cd1be6edb8c7f026e6ba240a53b594853624442b
Belinda Trotta <btrotta@users.noreply.github.com>
2023-12-03 17:47:44 +1100

    Day 3

--- Readme.md
+++ Readme.md
@@ -9,3 +9,10 @@ For part b, first reverse the line and the spelled words to efficiently iterate 
 
 ## Day 2
 Iterate over the lines. For part a, use a function to check each line so we can return early if the check fails.
+
+## Day 3
+For part a, iterate over the symbols, add each adjacent number to the total,
+and then replace its digits with `.` to avoid double-counting. 
+For part b, for each `*` symbol, make a list of all the numbers adjacent to it,  and calculate gear ratio when there are 
+only 2. It is not necessary to replace numbers with `.` in this case.
+
---
+++ day03a.py
@@ -0,0 +1,29 @@
+from utilities import *
+
+arr = parse_multi_string(sep="")
+
+ans = 0
+n_rows = len(arr)
+n_cols = len(arr[0])
+for row in range(n_rows):
+    for col in range(n_cols):
+        ch = arr[row][col]
+        if not(ch.isdigit()) and (ch != "."):
+            for i in [row - 1, row, row + 1]:
+                if (i >= 0) and (i < n_rows):
+                    for j in [col - 1, col, col + 1]:
+                        if (j >= 0) and (j < n_cols) and arr[i][j].isdigit():
+                            m = j - 1
+                            n = j + 1
+                            str_num = arr[i][j]
+                            while (m >= 0) and arr[i][m].isdigit():
+                                str_num = arr[i][m] + str_num
+                                arr[i][m] = "."
+                                m -= 1
+                            while (n < n_cols) and arr[i][n].isdigit():
+                                str_num = str_num + arr[i][n]
+                                arr[i][n] = "."
+                                n += 1
+                            ans += int(str_num)
+
+print(ans)
